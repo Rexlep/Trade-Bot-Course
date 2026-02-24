f6b9dca8daaad0d0455b42902de6b435fe20d511
Rexlep <141561659+Rexlep@users.noreply.github.com>
2026-02-24 13:47:24 +0330

    Coded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ccxt
 import pandas as pd
+import pytz
 
 exchange = ccxt.coinex()
 
@@ -9,7 +10,14 @@ timeframe = "1h"
 bars = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=100)
 
 df = pd.DataFrame(bars, columns=["timestamp", "open", "high", "low", "close", "volume"])
-df['timestamp'] = pd.to_datetime(df['timestamp'], unit="ms")
+df['timestamp'] = pd.to_datetime(df['timestamp'], unit="ms", utc=True)
+
+tehran_tz = pytz.timezone("Asia/Tehran")
+
+df['timestamp'] = df['timestamp'].dt.tz_convert(tehran_tz)
+df['timestamp'] = df['timestamp'].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+df.dropna(inplace=True)
 
 df.set_index("timestamp", inplace=True)
 print(df.head())
